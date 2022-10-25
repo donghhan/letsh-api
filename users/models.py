9b929a863768f42a6ae7c14f9aed6eeb8ad3ce27
donghhan <apensia914@gmail.com>
2022-10-25 21:08:27 +0900

    Included choices fields

--- users/models.py
+++ users/models.py
@@ -6,18 +6,33 @@ from common.models import *
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(
+        self,
+        email,
+        first_name,
+        last_name,
+        password=None,
+    ):
         if not email:
             raise ValueError(_("You must provide a valid email address."))
 
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.first_name = first_name
+        user.last_name = last_name
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
-        user = self.create_user(email, password=password)
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_user(
+            email, password=password, first_name=first_name, last_name=last_name
+        )
+        user.first_name = first_name
+        user.last_name = last_name
         user.is_admin = True
         user.save(using=self._db)
         return user
@@ -26,6 +41,19 @@ class MyUserManager(BaseUserManager):
 class User(AbstractBaseUser, TimeStampModel):
 
     """Custom User Definition Model"""
+
+    class SexChioces(models.TextChoices):
+        MALE = "male", _("Male")
+        FEMALE = "female", _("Female")
+        NOT_PREFER = "na", _("Prefer not to answer")
+
+    class LanguageChoices(models.TextChoices):
+        ENGLISH = "english", _("English")
+        THAI = "thai", _("Thai")
+
+    class CurrencyChoices(models.TextChoices):
+        USD = "usd", _("US Dollar")
+        THB = "thb", _("Thai Baht")
 
     email = models.EmailField(
         primary_key=True,
@@ -40,17 +68,17 @@ class User(AbstractBaseUser, TimeStampModel):
     first_name = models.CharField(
         max_length=100,
         verbose_name=_("First Name"),
+        blank=True,
         error_messages={
             "null": _("You should provide your first name."),
-            "blank": _("Your first name cannot be empty."),
         },
     )
     last_name = models.CharField(
         max_length=100,
         verbose_name=_("Last Name"),
+        blank=True,
         error_messages={
-            "null": _("You should provide your first name."),
-            "blank": _("Your first name cannot be empty."),
+            "null": _("You should provide your last name."),
         },
     )
     phone_number = models.CharField(
@@ -60,6 +88,27 @@ class User(AbstractBaseUser, TimeStampModel):
         blank=True,
         editable=False,
     )
+    profile_photo = models.ImageField(
+        null=True, blank=True, verbose_name=_("Profile Photo")
+    )
+    sex = models.CharField(
+        max_length=50,
+        verbose_name=(_("Sex")),
+        choices=SexChioces.choices,
+        default=SexChioces.MALE,
+    )
+    language = models.CharField(
+        max_length=10,
+        verbose_name=(_("Language")),
+        choices=LanguageChoices.choices,
+        default=LanguageChoices.ENGLISH,
+    )
+    currency = models.CharField(
+        max_length=10,
+        verbose_name=(_("Currency")),
+        choices=CurrencyChoices.choices,
+        default=CurrencyChoices.USD,
+    )
     is_host = models.BooleanField(default=False, verbose_name=_("Host"))
     is_active = models.BooleanField(default=True, verbose_name=_("Active User"))
     is_admin = models.BooleanField(default=False, verbose_name=_("Admin User"))
@@ -67,6 +116,7 @@ class User(AbstractBaseUser, TimeStampModel):
     objects = MyUserManager()
 
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
         return self.email
@@ -85,11 +135,17 @@ class User(AbstractBaseUser, TimeStampModel):
         return self.is_admin
 
     def clean(self):
-        if type(self.first_name) or type(self.last_name) != str:
+        # Check if either first name or last name includes characters other than alphabets.
+        if type(self.first_name) != str:
             raise ValidationError(
                 {
-                    "first_name": _("First name can only include characters."),
-                    "last_name": _("Last name can only include characters."),
+                    "first_name": _("First name can only include alphabets."),
+                }
+            )
+        elif type(self.last_name) != str:
+            raise ValidationError(
+                {
+                    "last_name": _("Last name can only include alphabets."),
                 }
             )
 
