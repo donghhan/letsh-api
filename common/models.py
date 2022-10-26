240701234ba08ebbb197619f73dfd562beeb1458
donghhan <apensia914@gmail.com>
2022-10-26 20:37:44 +0900

    Included room amenity abstract model

--- common/models.py
+++ common/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class TimeStampModel(models.Model):
@@ -8,6 +8,27 @@ class TimeStampModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class RoomOptions(models.Model):
+
+    """Room Option Model Definition"""
+
+    name = models.CharField(max_length=200, verbose_name=_("Name"))
+    icon = models.ImageField(null=True, blank=True)
+    description = models.CharField(
+        max_length=200,
+        verbose_name=_("Description"),
+        help_text=_("Short description of room option."),
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         abstract = True
