80e7503a0037bffea1ed0cabfda016d3da3a4194
donghhan <apensia914@gmail.com>
2022-11-01 11:31:44 +0900

    Included admin action

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from django import forms
 from django.utils.translation import gettext_lazy as _
 from .models import *
+
+
+@admin.action(description="Convert activity of selected users.")
+def set_activity(modeladmin, request, rooms):
+    for room in rooms.all():
+        print(room.is_active)
+        if room.is_active == False:
+            room.is_active = True
+            room.save()
+        else:
+            room.is_active = False
+            room.save()
 
 
 @admin.register(User)
@@ -9,4 +20,65 @@ class UserAdmin(admin.ModelAdmin):
 
     """User Admin Definition"""
 
-    list_display = ("first_name", "last_name")
+    list_display = (
+        "nickname",
+        "first_name",
+        "last_name",
+        "email",
+        "phone_number",
+        "sex",
+        "language",
+        "currency",
+        "is_host",
+        "is_active",
+    )
+    list_filter = (
+        "sex",
+        "language",
+        "currency",
+        "is_host",
+        "is_active",
+    )
+    list_per_page = 50
+    radio_fields = {"sex": admin.HORIZONTAL}
+    search_fields = ["nickname", "email", "phone_number"]
+    search_help_text = "Searching avilable with nickname, email and phone number."
+    fieldsets = (
+        (
+            "Personal Information",
+            {
+                "classes": "wide",
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "nickname",
+                    "sex",
+                    "phone_number",
+                ),
+            },
+        ),
+        (
+            "Preference",
+            {
+                "classes": "wide",
+                "fields": (
+                    "language",
+                    "currency",
+                ),
+            },
+        ),
+        (
+            "Status",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "is_host",
+                    "is_active",
+                    "is_admin",
+                ),
+            },
+        ),
+    )
+    empty_value_display = "(unknown)"
+    actions = [set_activity]
