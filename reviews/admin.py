b7bcc54063a36a14bdc3844709e5430e10d9a9ac
donghhan <apensia914@gmail.com>
2022-11-01 16:57:26 +0900

    Review admin customizing

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from .models import *
 
 
@@ -7,4 +8,27 @@ class ReviewAdmin(admin.ModelAdmin):
 
     """Review Admin Model"""
 
-    list_display = ("date", "review", "room")
+    list_display = (
+        "reviewer_nickname",
+        "date",
+        "review",
+        "room",
+        "cleanliness",
+        "communication",
+        "accuracy",
+        "get_individual_review_score",
+    )
+    list_filter = ("user__is_host", "cleanliness", "communication", "accuracy")
+    search_fields = ("room__name", "nickname")
+    search_help_text = "Searching avilable with room name and user's nickname."
+
+    def reviewer_nickname(self, obj):
+        return obj.user.nickname
+
+    reviewer_nickname.short_description = "nickname"
+
+    def get_individual_review_score(self, obj):
+        total_individual_score = obj.cleanliness + obj.communication + obj.accuracy
+        return round(total_individual_score / 3, 2)
+
+    get_individual_review_score.short_description = "total average score"
--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,11 @@
-from django.db import models
+from django.db.models import Model, QuerySet, Q, Manager
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.translation import gettext_lazy as _
 from users.models import *
 from accomodations.models import *
 
 
-class Review(models.Model):
+class Review(Model):
 
     """Review Model Definition"""
 
