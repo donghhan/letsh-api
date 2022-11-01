f97ec6c4edc1b3173ec7ec4d5a87054254c49d52
donghhan <apensia914@gmail.com>
2022-11-01 17:05:50 +0900

    Deleted RoomOption model

--- common/models.py
+++ common/models.py
@@ -11,24 +11,3 @@ class TimeStampModel(models.Model):
 
     class Meta:
         abstract = True
-
-
-class RoomOptions(models.Model):
-
-    """Room Option Model Definition"""
-
-    name = models.CharField(max_length=200, verbose_name=_("Name"))
-    icon = models.ImageField(null=True, blank=True)
-    description = models.CharField(
-        max_length=200,
-        verbose_name=_("Description"),
-        help_text=_("Short description of room option."),
-        null=True,
-        blank=True,
-    )
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        abstract = True
