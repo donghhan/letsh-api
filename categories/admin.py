0c125739d18157a1da8ba6ba8634474c7e72a4d1
donghhan <apensia914@gmail.com>
2022-11-03 13:22:26 +0900

    categories models and admin edited

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    """Category Admin Definition"""
+
+    list_display = ("name", "kind")
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from common.models import TimeStampModel
 
-# Create your models here.
+
+class Category(TimeStampModel):
+
+    """Category Model Definition"""
+
+    class CategoryKindChoices(models.TextChoices):
+        ROOM = ("room", "Room")
+        HOUSE = ("house", "House")
+        HOTEL_APARTMENT = ("hotel", "Hotel Apartments")
+        PARKING = ("parking", "Parking")
+        MOBILE = ("mobile", "Mobile")
+
+    name = models.CharField(max_length=200)
+    kind = models.CharField(
+        max_length=20,
+        choices=CategoryKindChoices.choices,
+        default=CategoryKindChoices.ROOM,
+    )
+
+    def __str__(self):
+        return f"{self.kind}: {self.name}"
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+        db_table = "categories"
