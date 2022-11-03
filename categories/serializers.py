78dd6dde193d8b3cbe4b5fc247c03da9797514d5
donghhan <apensia914@gmail.com>
2022-11-03 14:14:19 +0900

    Included category model serializer

---
+++ categories/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from .models import *
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
