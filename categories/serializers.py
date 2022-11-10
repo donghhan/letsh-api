b5aedb519e4326ba506634c412ab9ecb82adbffb
donghhan <apensia914@gmail.com>
2022-11-10 15:45:45 +0900

    Category detail PUT, DELETE functional views added

--- categories/serializers.py
+++ categories/serializers.py
@@ -9,3 +9,9 @@ class CategorySerializer(ModelSerializer):
 
     def create(self, validated_data):
         return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.kind = validated_data.get("kind", instance.kind)
+        instance.save()
+        return instance
