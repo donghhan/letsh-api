e4be63a2a5d5a3d9bc06ed76bec48aac656eab27
donghhan <apensia914@gmail.com>
2022-11-10 16:29:51 +0900

    Converted categories view into ModelViewSet

--- categories/urls.py
+++ categories/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [path("", categories), path("<int:pk>", category_detail)]
+urlpatterns = [
+    path("", CategoryViewSet.as_view({"get": "list", "post": "create"})),
+    path(
+        "<int:pk>",
+        CategoryViewSet.as_view(
+            {"get": "retrieve", "put": "partial_update", "delete": "destroy"}
+        ),
+    ),
+]
