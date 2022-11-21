534bb791833e61629a8d9ffaaa5ee27d49155503
donghhan <apensia914@gmail.com>
2022-11-21 10:39:50 +0900

    Hid SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rb(+qvjr93dk=8mr0568n7wggrfu)nc=@pev@+cw!=*)70!n*b"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
