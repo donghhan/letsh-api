d48b9563b92664be76ebc1bbad13146f3c56c035
donghhan <apensia914@gmail.com>
2022-12-22 17:08:00 +0900

    Added GET view of room photos

--- user_media/views.py
+++ user_media/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import NotFound, PermissionDenied
+from .serializers import *
 from .models import *
 
 
@@ -15,6 +16,11 @@ class PhotoDetailView(APIView):
             return Photo.objects.get(pk=pk)
         except Photo.DoesNotExist:
             raise NotFound
+
+    def get(self, request, pk):
+        photo = self.get_object(pk)
+        serializer = PhotoSerializer(photo)
+        return Response(serializer.data)
 
     def delete(self, request, pk):
         photo = self.get_object(pk)
