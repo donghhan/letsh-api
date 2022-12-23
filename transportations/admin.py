2f04c705f27f257e74020823a4eef3e122806ac6
donghhan <apensia914@gmail.com>
2022-12-23 15:33:07 +0900

    Edited admin panel for Features and Policy

--- transportations/admin.py
+++ transportations/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Features)
+class FeaturesAdmin(admin.ModelAdmin):
+
+    """Features Model Admin"""
+
+    list_display = ("feature_name", "icon")
+    list_per_page = 10
+    empty_value_display = "(unknown)"
+    search_fields = ("feature_name",)
+
+
+@admin.register(Policies)
+class PolicyAdmin(admin.ModelAdmin):
+
+    """Policy Model Admin"""
+
+    list_display = ("policy",)
+    list_per_page = 10
+    empty_value_display = "(unknown)"
+    search_fields = ("policy",)
+
+
+@admin.register(Transportation)
+class TransportationAdmin(admin.ModelAdmin):
+
+    """Transportation Model Admin"""
+
+    list_display = (
+        "name",
+        "price_per_night",
+        "seats",
+        "bag",
+        "number_of_doors",
+        "gear_type",
+        "car_type",
+        "features",
+        "policies",
+        "is_air_conditioned",
+    )
