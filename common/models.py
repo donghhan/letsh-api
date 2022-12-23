b0a3c9c563e0879d82cf27c59759968c86779f5b
donghhan <apensia914@gmail.com>
2022-12-23 15:31:08 +0900

    Deleted CommonNameModel

--- common/models.py
+++ common/models.py
@@ -11,18 +11,3 @@ class TimeStampModel(models.Model):
 
     class Meta:
         abstract = True
-
-
-class CommonNameModel(models.Model):
-
-    """Common Name Model Definition"""
-
-    """
-    This model is just for naming variety of choices such as
-    amenities, features, etc...
-    """
-
-    name = models.CharField(max_length=100, verbose_name=_("Name of item"))
-
-    def __str__(self):
-        return self.name
