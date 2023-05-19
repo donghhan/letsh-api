d761d0eba6eaf794d4b2a5009e86bc1683ae82aa
donghhan <apensia914@gmail.com>
2023-05-19 22:36:53 +0700

    User model & UserAdmin added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@register(User)
+class UserAdmin(UserAdmin):
+    empty_value_display = "None"
+    list_display = (
+        "username",
+        "email",
+        "get_full_name",
+        "mobile_number",
+        "sex",
+        "is_active",
+    )
+    list_filter = ("sex",)
+    list_per_page = 50
--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.admin import display
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """User Model Definition"""
+
+    class SexChoices(models.TextChoices):
+        MALE = "MALE", _("Male")
+        FEMALE = "FEMALE", _("Female")
+        UNKNOWN = "UNKNOWN", _("Unknown")
+
+    mobile_number = models.CharField(
+        max_length=128, verbose_name=_("Mobile Number"), null=True, blank=True
+    )
+    sex = models.CharField(
+        max_length=20,
+        verbose_name=_("Sex"),
+        choices=SexChoices.choices,
+        default=SexChoices.MALE,
+    )
+
+    def __str__(self):
+        return str(self.username)
+
+    @property
+    @display(description="Full Name")
+    def get_full_name(self):
+        return super().get_full_name()
