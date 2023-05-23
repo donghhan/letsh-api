f53976138d1bf30cd1260347b50a269d5f431f66
donghhan <apensia914@gmail.com>
2023-05-23 16:47:59 +0700

    Added custom permission for each views

--- rooms/views.py
+++ rooms/views.py
@@ -4,35 +4,61 @@ from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_200_OK
 from .serializers import *
 from .models import *
+from .permissions import *
 
 
 class RoomView(views.APIView):
+    permission_classes = [IsAdminOrReadOnly]
+
+    # Get all room information
     def get(self, request):
-        """
-        Get all room information
-        """
         all_rooms = Room.objects.all()
         serializer = RoomSerializer(all_rooms, many=True)
         return Response(serializer.data)
 
+    # Create new room
     def post(self, request):
-        """
-        Create new room
-        """
+        serializer = RoomSerializer(data=request.data)
+
+        if serializer.is_valid():
+            new_room = serializer.save()
+            return Response(RoomSerializer(new_room).data)
 
 
 class RoomDetailView(views.APIView):
-    def get(self):
-        pass
+    permission_classes = [IsAdminOrReadOnly]
 
-    def post(self):
-        pass
+    def get_object(self, pk):
+        try:
+            room = Room.objects.get(pk=pk)
+            return room
+        except Room.DoesNotExist:
+            return NotFound
 
-    def delete(self):
-        pass
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        serializer = RoomSerializer(room)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        room = self.get_object(pk)
+        serializer = RoomSerializer(room, data=request.data, partia=True)
+
+        if serializer.is_valid():
+            room_to_update = serializer.save()
+            return Response(RoomSerializer(room_to_update).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        room = self.get_object(pk)
+        room.delete()
+        return Response(status=HTTP_200_OK)
 
 
 class RoomAmenityView(views.APIView):
+    permission_classes = [IsAdminOrReadOnly]
+
     def get(self, request):
         all_amenities = RoomAmenity.objects.all()
         serializer = RoomAmenitySerializer(all_amenities, many=True)
@@ -47,6 +73,8 @@ class RoomAmenityView(views.APIView):
 
 
 class RoomAmenityDetailView(views.APIView):
+    permission_classes = [IsAdminOrReadOnly]
+
     def get_object(self, pk):
         try:
             amenity = RoomAmenity.objects.get(pk=pk)
