f533a89f140254263d9ec13e98ef08caaac6fe78
donghhan <apensia914@gmail.com>
2023-05-30 18:06:47 +0700

    Adding creating room test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,12 +1,13 @@
 from rest_framework import test
-from .models import RoomAmenity
+from .models import RoomAmenity, Room
+from users.models import User
 
 
 class RoomAmenityTest(test.APITestCase):
     class TestData:
         name = "Amenity test"
         icon = "example.png"
-        base_url = "/api/v1/rooms/amenities"
+        base_url = "/api/v1/rooms/amenities/"
 
     def setUp(self):
         RoomAmenity.objects.create(name=self.TestData.name, icon=self.TestData.icon)
@@ -25,13 +26,41 @@ class RoomAmenityTest(test.APITestCase):
         self.assertEqual(data[0]["name"], self.TestData.name)
 
     def test_create_new_amenities(self):
-        input = {"name": "New amenity", "icon": "new_icon.png"}
-        response = self.client.post(
-            self.TestData.base_url,
-            data=input,
-        )
-        data = response.json()
+        pass
 
-        self.assertEqual(response.status_code, 200, "Status code is not 200")
-        self.assertEqual(data["name"], input["name"])
-        self.assertEqual(data["icon"], input["icon"])
+
+class RoomTest(test.APITestCase):
+    class UserTestData:
+        username = "test1"
+        password = "Example123$"
+
+    class TestData:
+        name = "Test Room"
+        price_per_night = 100
+        guest = 4
+        base_url = "/api/v1/rooms/"
+        # User
+
+    def setUp(self):
+        # Room.objects.create(
+        #     name=self.TestData.name,
+        #     price_per_night=self.TestData.price_per_night,
+        #     guest=self.TestData.guest,
+        # )
+
+        user = User.objects.create(username=self.UserTestData.username)
+        user.set_password(self.UserTestData.password)
+        user.save()
+        self.user = user
+
+    def test_get_all_rooms(self):
+        pass
+
+    def test_create_room(self):
+        response = self.client.post(self.TestData.base_url)
+        self.assertEqual(response.status_code, 403)
+
+        self.client.force_login(self.user)
+        response_after_login = self.client.post(self.TestData.base_url)
+
+        print(response_after_login.json())
