4f206019c241bc72843e191e4a33fb872052f25c
donghhan <apensia914@gmail.com>
2023-05-31 13:32:43 +0700

    ChangePasswordView, Login/Logout View added

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("my-profile/", views.MyProfileView.as_view()),
+    path("change-password/", views.ChangePasswordView.as_view()),
+    path("login/", views.LoginView.as_view()),
+    path("logout/", views.LogoutView.as_view()),
     path("", views.UserView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework import views, status, viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -43,3 +44,56 @@ class UserView(views.APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class ChangePasswordView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        confirm_new_password = request.data.get("confirm_new_password")
+
+        if not old_password or not new_password or not confirm_new_password:
+            raise ParseError("You should fill out all details.")
+
+        if old_password == new_password:
+            raise ParseError(
+                "New password is too similar with old password. Try another one."
+            )
+
+        if new_password != confirm_new_password:
+            raise ParseError("New password and confirm_password do not match.")
+
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(views.APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            raise ParseError("You should provide both username and password.")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return Response({"ok": "Successfully logged in"})
+        else:
+            return Response({"error": "Wrong password"})
+
+
+class LogoutView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "Logged out successfully"})
