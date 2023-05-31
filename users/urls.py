caf56a031192b33a02a6ba671a615c1cce384cb2
donghhan <apensia914@gmail.com>
2023-05-31 10:39:51 +0700

    UserView added

--- users/urls.py
+++ users/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("my-profile/", views.MyProfileView.as_view())]
+urlpatterns = [
+    path("my-profile/", views.MyProfileView.as_view()),
+    path("", views.UserView.as_view()),
+]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import views, status, viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
 from .models import User
 from .serializers import *
 
@@ -20,6 +21,25 @@ class MyProfileView(views.APIView):
         if serializer.is_valid():
             updated_user = serializer.save()
             serializer = PrivateMyProfileSerializer(updated_user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class UserView(views.APIView):
+    def post(self, request):
+        password = request.data.get("password")
+
+        if not password:
+            raise ParseError("Password is required.")
+
+        serializer = PrivateMyProfileSerializer(data=request.data)
+
+        if serializer.is_valid():
+            new_user = serializer.save()
+            new_user.set_password(password)
+            new_user.save()
+            serializer = PrivateMyProfileSerializer(new_user)
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
