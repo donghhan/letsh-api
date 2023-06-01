f58f4835a64ac0a0675e0c9995cbde33ca563b37
donghhan <apensia914@gmail.com>
2023-06-01 14:07:45 +0700

    Inserted reservation_id & improved admin panel

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,14 +1,82 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from .models import *
 
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = (
-        "guest",
+    empty_value_display = "-----"
+    list_display = [
+        "reservation_id",
+        "get_guest_full_name",
+        "get_guest_mobile_phone",
+        "get_guest_email",
         "room",
         "check_in",
         "check_out",
         "adult",
         "children",
-    )
+    ]
+    readonly_fields = [
+        "reservation_id",
+        "reservation_created_time",
+        "room",
+        "get_total_stay_length",
+    ]
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ["reservation_id", "reservation_created_time"],
+                "classes": ["wide"],
+            },
+        ),
+        (
+            "Guest Information",
+            {
+                "fields": ["guest", "adult", "children"],
+            },
+        ),
+        (
+            "Room Information",
+            {
+                "fields": ["room"],
+            },
+        ),
+        (
+            "Reservation Time",
+            {
+                "fields": ["check_in", "check_out", "get_total_stay_length"],
+            },
+        ),
+    ]
+
+    @admin.display(description="Reservation Created Time")
+    def reservation_created_time(self, obj):
+        return obj.created_at
+
+    def get_guest_full_name(self, obj):
+        full_name = obj.guest.get_full_name
+        return full_name
+
+    get_guest_full_name.short_description = "Name"
+
+    def get_guest_mobile_phone(self, obj):
+        mobile_number = obj.guest.mobile_number
+        return mobile_number
+
+    get_guest_mobile_phone.short_description = "Mobile Number"
+
+    def get_guest_email(self, obj):
+        email = obj.guest.email
+        return email
+
+    get_guest_email.short_description = "Email"
+
+    def get_total_stay_length(self, obj):
+        check_in = obj.check_in
+        check_out = obj.check_out
+        total_stay_length = (check_out - check_in).days
+        return f"{total_stay_length} " + _("days")
+
+    get_total_stay_length.short_description = "Total Staying Length"
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,6 @@
+import uuid
+from datetime import datetime
+from django.core.validators import MinValueValidator
 from django.utils.timezone import now
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -8,12 +11,24 @@ class Reservation(CommonDateTimeModel, models.Model):
 
     """Reservation Model Definition"""
 
+    def reservation_id_generator():
+        reservation_created_time = str(datetime.now().date()).replace("-", "")
+        reservation_code = uuid.uuid4().hex[:6].upper()
+        reservation_id = reservation_created_time + reservation_code
+        return reservation_id
+
+    reservation_id = models.SlugField(
+        unique=True,
+        primary_key=True,
+        editable=False,
+        max_length=20,
+        default=reservation_id_generator(),
+    )
     guest = models.ForeignKey(
         "users.User",
         on_delete=models.CASCADE,
-        null=True,
-        blank=True,
         verbose_name=_("Reserved guest"),
+        related_name="reservations",
     )
     room = models.ForeignKey(
         "rooms.Room",
@@ -21,9 +36,10 @@ class Reservation(CommonDateTimeModel, models.Model):
         null=True,
         blank=True,
         verbose_name=_("Room"),
+        related_name="reservations",
     )
     adult = models.PositiveSmallIntegerField(
-        verbose_name=_("Number of adults"), default=2
+        verbose_name=_("Number of adults"), validators=[MinValueValidator(1)], default=2
     )
     children = models.PositiveSmallIntegerField(
         verbose_name=_("Number of children"), null=True, blank=True, default=0
@@ -32,7 +48,7 @@ class Reservation(CommonDateTimeModel, models.Model):
     check_out = models.DateField(verbose_name=_("Check-out date"))
 
     def __str__(self):
-        return str(self.guest.username)
+        return str(self.reservation_id)
 
     class Meta:
         verbose_name = _("Reservation")
