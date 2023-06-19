0ad8a0efebc7ce0609964e704bdba53ceb897703
donghhan <apensia914@gmail.com>
2023-06-19 14:43:18 +0700

    python-dotenv incompatibility issue fixing

--- config/settings.py
+++ config/settings.py
@@ -13,12 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-from dotenv import load_dotenv
 from django.utils.translation import gettext_lazy as _
 import dj_database_url
-
-
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
