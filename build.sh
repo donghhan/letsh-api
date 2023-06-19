9d619ca051f02ba5b416ff1a69253ce12adc7aab
donghhan <apensia914@gmail.com>
2023-06-19 15:05:04 +0700

    Changing order of installing setuptools

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 # exit on error
 set -o errexit
 
+poetry install
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
-
-poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
