0c803cd5fe2b171fa54b982c6dbff38cec1d1b2f
donghhan <apensia914@gmail.com>
2023-06-25 17:01:56 +0700

    Changed MEDIA_URL

--- config/settings.py
+++ config/settings.py
@@ -183,18 +183,10 @@ if not DEBUG:
 
 # Media files upload
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
